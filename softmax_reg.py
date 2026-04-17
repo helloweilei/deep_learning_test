7826be71052c11fc1d89c3a48fb1552f9edeafc8
魏磊 <1060290944@qq.com>
2026-04-17 11:44:38 +0800

    update

--- softmax_reg.py
+++ softmax_reg.py
@@ -11,22 +11,27 @@ num_outputs = 10
 W = torch.normal(0, 0.01, size=(num_inputs, num_outputs), requires_grad=True)
 b = torch.zeros(num_outputs, requires_grad=True)
 
+
 def softmax(X):
     X_exp = torch.exp(X)
     partition = X_exp.sum(dim=1, keepdim=True)
     return X_exp / partition
 
+
 def net(X):
     return softmax(torch.matmul(X.reshape(-1, W.shape[0]), W) + b)
 
+
 def cross_entropy(y_hat, y):
     return -torch.log(y_hat[range(len(y_hat)), y])
+
 
 def accuracy(y_hat, y):
     if len(y_hat.shape) > 1 and y_hat.shape[1] > 1:
         y_hat = y_hat.argmax(dim=1)
     cmp = y_hat.type(y.dtype) == y
     return float(cmp.type(y.dtype).sum())
+
 
 def evaluate_accuracy(net, data_iter):
     if isinstance(net, torch.nn.Module):
@@ -37,7 +42,9 @@ def evaluate_accuracy(net, data_iter):
             metric.add(accuracy(net(X), y), y.numel())
     return metric[0] / metric[1]
 
+
 # print(evaluate_accuracy(net, test_iter))
+
 
 def train_epoch_ch3(net, train_iter, loss, updater):
     if isinstance(net, torch.nn.Module):
@@ -57,16 +64,40 @@ def train_epoch_ch3(net, train_iter, loss, updater):
         metric.add(float(l.sum().detach()), accuracy(y_hat, y), y.numel())
     return metric[0] / metric[2], metric[1] / metric[2]
 
+
 class Animator:
-    def __init__(self, xlabel=None, ylabel=None, legend=None, xlim=None, ylim=None, xcale=1, yscale=1, fmts=('-', 'm--', 'g-.', 'r:'), nrows=1, ncols=1, figsize=(3.5, 2.5)):
+    def __init__(
+        self,
+        xlabel=None,
+        ylabel=None,
+        legend=None,
+        xlim=None,
+        ylim=None,
+        xcale=1,
+        yscale=1,
+        fmts=("-", "m--", "g-.", "r:"),
+        nrows=1,
+        ncols=1,
+        figsize=(3.5, 2.5),
+    ):
         if legend is None:
             legend = []
         self.fig, self.axes = plt.subplots(nrows, ncols, figsize=figsize)
         plt.ion()
         if nrows * ncols == 1:
-            self.axes = [self.axes, ]
+            self.axes = [
+                self.axes,
+            ]
 
-        self.config_axes = lambda: self.axes[0].set_xlabel(xlabel) or self.axes[0].set_ylabel(ylabel) or self.axes[0].set_xlim(xlim) or self.axes[0].set_ylim(ylim) or self.axes[0].legend(legend) or self.axes[0].set_xscale(xcale) or self.axes[0].set_yscale(yscale)
+        self.config_axes = lambda: (
+            self.axes[0].set_xlabel(xlabel)
+            or self.axes[0].set_ylabel(ylabel)
+            or self.axes[0].set_xlim(xlim)
+            or self.axes[0].set_ylim(ylim)
+            or self.axes[0].legend(legend)
+            or self.axes[0].set_xscale(xcale)
+            or self.axes[0].set_yscale(yscale)
+        )
         self.X, self.Y, self.fmts = None, None, fmts
 
     def add(self, x, y):
@@ -91,6 +122,7 @@ class Animator:
         plt.pause(0.001)
         plt.show(block=False)
 
+
 def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
     # animator = Animator(xlabel='epoch', xlim=[1, num_epochs], ylim=[0.3, 0.9], legend=['train loss', 'train acc', 'test acc'])
     for epoch in range(num_epochs):
@@ -98,19 +130,34 @@ def train_ch3(net, train_iter, test_iter, loss, num_epochs, updater):
         test_acc = evaluate_accuracy(net, test_iter)
         # animator.add(epoch + 1, train_metrics + (test_acc,))
 
+
 lr = 0.1
+
+
 def updater(batch_size):
     return sgd([W, b], lr, batch_size)
 
+
 num_epochs = 10
-train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+net1 = torch.nn.Sequential(torch.nn.Flatten(), torch.nn.Linear(num_inputs, num_outputs))
+def init_weights(m):
+    if type(m) == torch.nn.Linear:
+        torch.nn.init.normal_(m.weight, std=0.01)
+net1.apply(init_weights)
+loss = torch.nn.CrossEntropyLoss(reduction='none')
+optimizer = torch.optim.SGD(net1.parameters(), lr=lr)
+train_ch3(net1, train_iter, test_iter, loss, num_epochs, optimizer)
+#train_ch3(net, train_iter, test_iter, cross_entropy, num_epochs, updater)
+
+
 def predict_ch3(net, test_iter, n=6):
     for X, y in test_iter:
         break
     trues = get_fashion_mnist_labels(y)
     preds = get_fashion_mnist_labels(net(X).argmax(dim=1))
-    titles = [true + '\n' + pred for true, pred in zip(trues, preds)]
-    show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n])
+    titles = [true + "\n" + pred for true, pred in zip(trues, preds)]
+    show_images(X[0:n].reshape((n, 28, 28)), 1, n, titles=titles[0:n], scale=2.5)
     plt.show(block=True)
+
 
 predict_ch3(net, test_iter)
